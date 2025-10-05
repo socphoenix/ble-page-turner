b2f930ed67c5d157ed51ceb450589b69601e4a01
socphoenix <tuc03516@gmail.com>
2025-10-05 06:16:11 -0600

    change to add deep sleep for li-on battery
    The AA batteries only worked with non-rechargeable batteries. In order to use this more sustainably, switched to li-on battery, and added deep sleep and a power indicator using the built in LED.

--- esp32-page-turner.ino
+++ esp32-page-turner.ino
@@ -17,24 +17,44 @@
 #include <BleKeyboard.h>
 #include "driver/rtc_io.h"
 
-BleKeyboard bleKeyboard("bt page turner");
+BleKeyboard bleKeyboard("MBs page turner");
 #define back 4
 #define next 2
+#define wakeup_back GPIO_NUM_4
+#define wakeup_next GPIO_NUM_2
+//#define BUTTON_PIN_BITMASK ((1ULL >> 2) | (1ULL >> 4))
+#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)
+uint64_t bitmask = BUTTON_PIN_BITMASK(wakeup_back) | BUTTON_PIN_BITMASK(wakeup_next);
+#define USE_EXT0_WAKEUP 0
+
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(back, INPUT_PULLUP);
   pinMode(next, INPUT_PULLUP);
+  pinMode(22, OUTPUT);
+  digitalWrite(22, LOW);
   bleKeyboard.begin();
+  esp_sleep_enable_ext1_wakeup_io(bitmask, ESP_EXT1_WAKEUP_ALL_LOW);
+  rtc_gpio_pullup_en(wakeup_back);
+  rtc_gpio_pulldown_dis(wakeup_back);
+  rtc_gpio_pullup_en(wakeup_next);
+  rtc_gpio_pulldown_dis(wakeup_next);
+  delay(1000);
 }
 
 void loop() {
-  if(bleKeyboard.isConnected() && digitalRead(back) == 0) {
-    bleKeyboard.write(218);
+  if(digitalRead(back) == 0 && digitalRead(next) == 0) {
+    digitalWrite(22, HIGH);
     delay(1000);
+    esp_deep_sleep_start();
   }
   else if(bleKeyboard.isConnected() && digitalRead(next) == 0) {
     bleKeyboard.write(217);
+    delay(1000);
+  }
+  else if(bleKeyboard.isConnected() && digitalRead(back) == 0) {
+    bleKeyboard.write(218);
     delay(1000);
   }
 }
